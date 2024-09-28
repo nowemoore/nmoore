3e90a7b4719e78f448d459eadd2646ced8c3558c
n1xd <nikola.v.datkova@gmail.com>
2024-09-28 13:30:22 -0400

    afeb

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="footer-left">
-                <p>London, UK</p> {/* Adjust location text as needed */}
+                <p>Philadelphia, US</p> {/* Adjust location text as needed */}
             </div>
             <div className="footer-right">
                 <img src={signatureImage} alt="Signature" className="footer-signature" />
